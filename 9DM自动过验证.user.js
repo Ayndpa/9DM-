44225ecf23ea26e681ef735ef40b758170583e2c
Ji Fuyao <abcxiaoyao1234@163.com>
2025-01-01 14:47:48 +0800

    Update 9DM自动过验证.user.js

--- 9DM自动过验证.user.js
+++ 9DM自动过验证.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动计算并提交算术题
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  自动计算并提交9dmgamemod.com及其子域名上的算术题
 // @author       你的名字
 // @match        *://*.9dmgamemod.com/*
